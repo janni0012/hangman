283d859c07715142acb05e8326c4139e57fc2525
janni0012 <51456403+janni0012@users.noreply.github.com>
2019-06-30 00:16:07 +0200

    Finished the guess function and added a function to check if there are enough guesses left

--- hangman.py
+++ hangman.py
@@ -9,12 +9,38 @@ class Hangman(object):
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.word = word
+        self.length = len(word)
+        self.mask = "_"*self.length
+        self.guesses = []
 
+    def failcheck(self):
+        self.remaining_guesses -= 1
+        if self.remaining_guesses <0:
+            self.status = STATUS_LOSE
+                
     def guess(self, char):
-        pass
+        if self.status == STATUS_ONGOING:
+            if char in self.guesses:
+                self.failcheck()
+            else:
+                self.guesses.append(char)
+                if char in self.word:
+                    for inc in range(self.length):
+                        if self.word[inc] == char:
+                            self.mask = self.mask[:inc]+char+self.mask[inc+1:]
+                            if "_" not in self.mask:
+                                self.status = STATUS_WIN
+                else: 
+                    self.failcheck()
+        else: 
+            raise ValueError("No guesses left!")
+                    
+
+                
 
     def get_masked_word(self):
-        pass
+        return self.mask
 
     def get_status(self):
-        pass
+        return self.status
